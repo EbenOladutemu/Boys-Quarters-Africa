43ddf63dd6e744107d12b312ce44902915b54f69
EbenDipo <e.oladutemu@taxtech.com.ng>
2019-08-12 11:21:38 +0100

    Updated Team members

--- js/vuescripts.js
+++ js/vuescripts.js
@@ -350,83 +350,9 @@
                 </div>
                 <div class="col-sm-4">
                   <div class="team-member">
-                    <img class="mx-auto rounded-circle" src="img/team/Oyindamola-Ejiro.JPG" alt="Oyindamola Ejiro">
-                    <h4>Oyindamola Ejiro Ohwesi</h4>
-                    <p class="text-muted">Lead, Communications & Content Development</p>
-                    <ul class="list-inline social-buttons">
-                    <li class="list-inline-item bg-facebook">
-                      <a href="#">
-                        <i class="fab fa-facebook-f"></i>
-                      </a>
-                    </li>
-                    <li class="list-inline-item">
-                      <a href="https://www.instagram.com/ohweskid/" class="bg-instagram">
-                        <i class="fab fa-instagram"></i>
-                      </a>
-                    </li>
-                    <li class="list-inline-item bg-twitter">
-                      <a href="#">
-                        <i class="fab fa-twitter"></i>
-                      </a>
-                    </li>
-                  </ul> 
-                  </div>
-                </div>
-                <div class="col-sm-4">
-                  <div class="team-member">
-                    <img class="mx-auto rounded-circle" src="img/team/Louis-Okoh.JPG" alt="Louis Okoh">
-                    <h4>Louis Okoh</h4>
-                    <p class="text-muted">Lead, Project Management</p><br>
-                    <ul class="list-inline social-buttons">
-                    <li class="list-inline-item bg-facebook">
-                      <a href="https://www.facebook.com/sunday.louis.7">
-                        <i class="fab fa-facebook-f"></i>
-                      </a>
-                    </li>
-                    <li class="list-inline-item">
-                      <a href="https://www.instagram.com/okoh_louis" class="bg-instagram">
-                        <i class="fab fa-instagram"></i>
-                      </a>
-                    </li>
-                    <li class="list-inline-item bg-twitter">
-                      <a href="https://twitter.com/okoh_louis">
-                        <i class="fab fa-twitter"></i>
-                      </a>
-                    </li>
-                  </ul> 
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-sm-4">
-                  <div class="team-member">
-                    <img class="mx-auto rounded-circle" src="img/team/Ebenezer-Oladutemu.JPG" alt="Ebenezer Oladutemu">
-                    <h4>Ebenezer Oladutemu</h4>
-                    <p class="text-muted">Strategy/Project Manager</p><br>
-                    <ul class="list-inline social-buttons">
-                    <li class="list-inline-item bg-facebook">
-                      <a href="https://www.facebook.com/oladutemuebenezer">
-                        <i class="fab fa-facebook-f"></i>
-                      </a>
-                    </li>
-                    <li class="list-inline-item">
-                      <a href="https://www.instagram.com/oladutemu_ebenezer" class="bg-instagram">
-                        <i class="fab fa-instagram"></i>
-                      </a>
-                    </li>
-                    <li class="list-inline-item bg-twitter">
-                      <a href="https://twitter.com/oladutemueben">
-                        <i class="fab fa-twitter"></i>
-                      </a>
-                    </li>
-                  </ul> 
-                  </div>
-                </div>
-                <div class="col-sm-4">
-                  <div class="team-member">
                     <img class="mx-auto rounded-circle" src="img/team/Glory-Odikagbue.JPG" alt="Glory Odikagbue">
                     <h4>Glory Odikagbue</h4>
-                    <p class="text-muted">Project Support Officer/Volunteer Mobilization</p>
+                    <p class="text-muted">Chief Operations Officer</p>
                     <ul class="list-inline social-buttons">
                     <li class="list-inline-item bg-facebook">
                       <a href="https://www.facebook.com/odikagbue.funayaglory">
@@ -448,9 +374,107 @@
                 </div>
                 <div class="col-sm-4">
                   <div class="team-member">
+                    <img class="mx-auto rounded-circle" src="img/team/Tomi-Fowe.JPEG" alt="Tomi Fowe">
+                    <h4>Oluwatomidimu Fowe</h4>
+                    <p class="text-muted">Lead, Finance</p>
+                    <ul class="list-inline social-buttons">
+                    <li class="list-inline-item bg-facebook">
+                      <a href="#">
+                        <i class="fab fa-facebook-f"></i>
+                      </a>
+                    </li>
+                    <li class="list-inline-item">
+                      <a href="https://www.instagram.com/ohweskid/" class="bg-instagram">
+                        <i class="fab fa-instagram"></i>
+                      </a>
+                    </li>
+                    <li class="list-inline-item bg-twitter">
+                      <a href="#">
+                        <i class="fab fa-twitter"></i>
+                      </a>
+                    </li>
+                  </ul> 
+                  </div>
+                </div>
+                <div class="col-sm-4">
+                  <div class="team-member">
                     <img class="mx-auto rounded-circle" src="img/team/Kolade-Emmanuel.JPG" alt="Kolade Emmanuel">
                     <h4>Kolade Emmanuel</h4>
-                    <p class="text-muted">Lead, Logistics</p><br>
+                    <p class="text-muted">Lead, Programmes Management</p><br>
+                    <ul class="list-inline social-buttons">
+                    <li class="list-inline-item bg-facebook">
+                      <a href="https://www.facebook.com/Emmkolade">
+                        <i class="fab fa-facebook-f"></i>
+                      </a>
+                    </li>
+                    <li class="list-inline-item">
+                      <a href="https://www.instagram.com/emmakolade" class="bg-instagram">
+                        <i class="fab fa-instagram"></i>
+                      </a>
+                    </li>
+                    <li class="list-inline-item bg-twitter">
+                      <a href="https://twitter.com/emmakolade">
+                        <i class="fab fa-twitter"></i>
+                      </a>
+                    </li>
+                  </ul> 
+                  </div>
+                </div>
+                <div class="col-sm-4">
+                  <div class="team-member">
+                    <img class="mx-auto rounded-circle" src="img/team/Adetomiwa-Badewole.JPEG" alt="Adetomiwa Badewole">
+                    <h4>Adetomiwa Badewole</h4>
+                    <p class="text-muted">Lead, Communications</p><br>
+                    <ul class="list-inline social-buttons">
+                    <li class="list-inline-item bg-facebook">
+                      <a href="https://www.facebook.com/sunday.louis.7">
+                        <i class="fab fa-facebook-f"></i>
+                      </a>
+                    </li>
+                    <li class="list-inline-item">
+                      <a href="https://www.instagram.com/okoh_louis" class="bg-instagram">
+                        <i class="fab fa-instagram"></i>
+                      </a>
+                    </li>
+                    <li class="list-inline-item bg-twitter">
+                      <a href="https://twitter.com/okoh_louis">
+                        <i class="fab fa-twitter"></i>
+                      </a>
+                    </li>
+                  </ul> 
+                  </div>
+                </div>
+                <div class="col-sm-4">
+                  <div class="team-member">
+                    <img class="mx-auto rounded-circle" src="img/team/Ebenezer-Oladutemu.JPG" alt="Ebenezer Oladutemu">
+                    <h4>Ebenezer Oladutemu</h4>
+                    <p class="text-muted">Lead, Community and Civic Engagement</p><br>
+                    <ul class="list-inline social-buttons">
+                    <li class="list-inline-item bg-facebook">
+                      <a href="https://www.facebook.com/oladutemuebenezer">
+                        <i class="fab fa-facebook-f"></i>
+                      </a>
+                    </li>
+                    <li class="list-inline-item">
+                      <a href="https://www.instagram.com/oladutemu_ebenezer" class="bg-instagram">
+                        <i class="fab fa-instagram"></i>
+                      </a>
+                    </li>
+                    <li class="list-inline-item bg-twitter">
+                      <a href="https://twitter.com/oladutemueben">
+                        <i class="fab fa-twitter"></i>
+                      </a>
+                    </li>
+                  </ul> 
+                  </div>
+                </div>
+                
+                
+                <div class="col-sm-12">
+                  <div class="team-member">
+                    <img class="mx-auto rounded-circle" src="img/team/Kolade-Emmanuel.JPG" alt="Kolade Emmanuel">
+                    <h4>Isaac Kanu</h4>
+                    <p class="text-muted">Lead, Research and Data Intelligence</p><br>
                     <ul class="list-inline social-buttons">
                     <li class="list-inline-item bg-facebook">
                       <a href="https://www.facebook.com/Emmkolade">
