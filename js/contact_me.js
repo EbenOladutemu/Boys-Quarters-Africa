55039b691c92971ba03fbe2c4339a400efa4bf5d
EbenDipo <e.oladutemu@taxtech.com.ng>
2019-08-16 15:32:38 +0100

    Changed AJAX url

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././mail/contact_me.php",
+        url: "https://github.com/OladutemuEben/Boys-Quarters-Africa/blob/master/mail/contact_me.php",
         type: "POST",
         data: {
           name: name,
