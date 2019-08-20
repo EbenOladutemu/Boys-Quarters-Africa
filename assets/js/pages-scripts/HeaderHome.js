43b47ca01ad7834aea3501c079fa7f17e5595ccf
EbenDipo <e.oladutemu@taxtech.com.ng>
2019-08-20 17:53:17 +0100

    Corrected Learn More link

--- assets/js/pages-scripts/HeaderHome.js
+++ assets/js/pages-scripts/HeaderHome.js
@@ -9,7 +9,7 @@ Vue.component('header-home',{
           <!-- Visible only on xs, Visible only on sm -->
           <div class="intro-lead-in d-block d-sm-none d-none d-sm-block d-md-none pad-sm text-uppercase text-bold" style="font-size: 17px">We’ve been commissioned to educate, empower & advocate for a new tribe of Boys who will deliberately & purposefully transition from Boyhood to Manhood.</div>
           <div class="intro-heading text-uppercase"></div>
-          <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger learn-mg" href="about-us">Learn More</a>
+          <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger learn-mg" href="who-we-are/about-us">Learn More</a>
         </div>
       </div>
     </header>`
