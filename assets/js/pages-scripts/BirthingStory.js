fc94c7f1683da8338d1c13ec9369e1b13db39905
EbenDipo <e.oladutemu@taxtech.com.ng>
2019-08-20 19:43:10 +0100

    Update Home page

---
+++ assets/js/pages-scripts/BirthingStory.js
@@ -0,0 +1,167 @@
+Vue.component('birthing-story',{
+	template: `<section id="about" style="padding-top: 50px; padding-bottom:0px">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <h2 class="section-heading text-uppercase">About US</h2><br>
+          <!-- <h3 class="section-subheading text-muted"></h3> -->
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <ul class="timeline">
+            <li>
+              <div class="timeline-image img-1">
+                <img class="rounded-circle img-fluid" src="assets/img/about/about-1.jpeg" alt="Boys Quarter Africa">
+              </div>
+              <div class="timeline-panel about-1">
+                <div class="timeline-heading">
+                  <!-- <h4>2009-2011</h4> -->
+                  <h4 class="subheading text-uppercase">Boys Need Attention Too</h4>
+                </div>
+                <div class="timeline-body">
+                  <p>From the boy with an absent Father in Kampala, to the boy who has just been sexually molested in Abuja, the life trajectory of the African boy is filled with valleys, mountains and divergent experiences they may not be able to share until they become Fathers. Hence, this destructive weight is passed on from generation to generations and from Fathers to their Sons.</p><br>
+
+                  <p>Africa as a continent is encumbered by divergent warring darkness and the need to salvage the boy race must be an important focus, if we must deliver the future we desire for Africa & humanity. It would fatal to assume the movement toward the redemption of the boy-child race is another cross fire in the feminism & anti-feminism war. The Future is of both gender – Male & Female.
+                  </p><br>
+                    <p>There are countless<span id="dots">...</span><span id="more"> courageous falsehood about Manhood and it is quite difficult especially in Africa. How do you explain the calamity of Men who have accidentally found themselves in a society that demands from them what hasn’t been invested, the emotional trauma they undergo being tagged as the less emotional being and the burden of living a lie all through their short life?<br><br>
+
+                    The assumptions that Men don’t cry, men handle challenges better, they are macho and powerful are part of the underlying root of the Men Crisis in Africa and beyond. It is even said by several studies that Men don’t want love, judging by the masculine craving for respect and honour. Therefore, we have emotionally broken, psychologically battered and mentally inconsistent men who raise men of their kinds.<br><br>
+
+                    The race to rescue the Male Gender isn’t a hundred-meter dash but marathon.<br><br>
+                    If we spend all of time raising the feminism and gender equality flags, boosting the esteems of our girls and they end up in the unsecured arms of irresponsible young men, either as husbands, fathers, neighbour, bosses or colleagues, then all the efforts may end up wasted.<br><br>
+
+                    While the world is busy helping, grooming and challenging young African girls to be independent, godly, strong, responsible and accountable, we have left the young boys in the hands of friends, musical videos and the already-endangered society.<br><br>
+
+                    The Boys Quarters Africa movement isn’t another gender inequality argument but gone are the days when we had responsible boys in our schools, mosques, churches and the society at large. The depth of their state of mind is shown in the speedy quest for money without any legal cum productive activity and it is sad to say we may be grooming and raising a group of Showbiz set of Men, whose hearts are far from God, sold out to mammon and lacks usefulness on every front.<br><br> 
+
+                    We believe Africa is our home and we must deliver her.</span></p>
+                    <button class="btn btn-primary" onclick="myFunction()" id="myBtn">Read More</button>
+                </div>
+              </div>
+            </li>
+            <li class="timeline-inverted">
+              <div class="timeline-image img-1">
+                <img class="rounded-circle img-fluid" src="assets/img/about/about-2.jpg" alt="Boys Quarter Africa">
+              </div>
+              <div class="timeline-panel">
+                <div class="timeline-heading">
+                  <h4 class="subheading text-uppercase">THE BIRTHING EXPERIENCE</h4>
+                  <h6 class="subheading"><i>Solomon O. Ayodele – Lead Execution Officer, Boys Quarters Africa</i></h6>
+                </div>
+                <div class="timeline-body">
+                  <p>After reading the Dignity of Manhood by Gbile Akanni for the second time in 2014, the passion to see young boys live out the fullness of destiny became a tingling and subtle burden. The book re-opened my heart to the gap in the boy-child space and need for MEN to stand in their place in Destiny. However, I have in my personal space reached out to a couple of young boys, encouraged them and challenged them on divergent front through one-on-one mentorship and other religious platforms.
+                  </p><br>
+                    <p>It became crystal clear<span id="dots2">...</span><span id="more2"> in June 2017 when I travelled to Babcock University for the convocation of a family member. We were trying to get a spot under the large canopy when a Governor in the South-Western part of Nigeria started his sincere lamentation about the proportion of the dwindling academic performance of the young men in our schools. The crux of the matter was that, out of the students who made first class degrees at the convocation, the ladies got over 90% of the certificate, awards and recognition.<br><br>
+                    
+                    This isn’t about academic performance but a slight reflection of the current trends. Go to our churches and mosques, you’ll find out how ‘responsible’ the boys are, strolling away from little commitments, choosing to engage in frivolous activities.<br><br> 
+
+                    The Book, the several lamentations and the current realties has resonated to a call to action.<br></span></p>
+                    <button class="btn btn-primary" onclick="myFunction2()" id="myBtn2">Read More</button>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="timeline-image">
+                <img class="rounded-circle img-fluid" src="assets/img/about/about-3.jpeg" alt="Boys Quarter Africa">
+              </div>
+              <div class="timeline-panel about-1">
+                <div class="timeline-heading">
+                  <!-- <h4>December 2012</h4> -->
+                  <h4 class="subheading">THE VISION</h4>
+                </div>
+                <div class="timeline-body">
+                  <p>To raise, challenge, encourage and inspire Boys who will be strong enough to submit to godliness, fierce enough to be fearless in the face of the usual challenges, humble enough to be responsible; culminating into responsible fathers, reliable husbands, accountable Sons and great citizens.
+                  </p><br>
+                  <div class="timeline-heading">
+                    <h4 class="subheading">The Vision Tagline</h4>
+                    <p>Intentional and deliberate transition from Boyhood to Manhood.</p>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li class="timeline-inverted">
+              <div class="timeline-image">
+                <img class="rounded-circle img-fluid" src="assets/img/about/about-4.png" alt="Boys Quarter Africa">
+              </div>
+              <div class="timeline-panel">
+                <div class="timeline-heading">
+                  <!-- <h4>July 2014</h4> -->
+                  <h4 class="subheading text-uppercase">The Mission Statement</h4>
+                </div>
+                <div class="timeline-body">
+                  <p>Be an inspiring bridge from boyhood to manhood, by redefining the essence of masculinity through godly mentorship, exposure to role models, mental cum emotional build-ups and connections that will birth destiny fulfilment.
+                  </p>
+                    <ul><span id="dots3"></span><span id="more3">
+                      <li class="disc">The vision is to reach out to 2,000 young African boys by 11:59pm on the 31st of December 2028, leveraging on other existing platforms, NGOs and other structured bodies who may be able to take Boys in, into corrective facilities.</li>
+
+                      <li class="disc">The vision is to reach out to 2,000 young African boys by 11:59pm on the 31st of December 2028, leveraging on other existing platforms, NGOs and other structured bodies who may be able to take Boys in, into corrective facilities.</li>
+
+                      <li class="disc">Build correctional facilities across the major cities in Africa, where young boys are withdrawn from the street and exposed to well-grounded counselling and psychological mop-ups</li>
+
+                      <li class="disc">Build a Boys-Only Innovation Hub across 40 cities in Africa
+                      Young boys across Africa will be granted scholarships, depending on the prevailing challenges. Which could range from deceased parents, single parents etc.</li><br>
+
+                      <p>Young boys across Africa will be granted scholarships, depending on the prevailing challenges. Which could range from deceased parents, single parents, etc.</p></span>
+                    </ul>
+
+                    <button class="btn btn-primary" onclick="myFunction3()" id="myBtn3">Read More</button>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="timeline-image">
+                <img class="rounded-circle img-fluid" src="assets/img/about/bqafrica2.jpg" alt="Boys Quarter Africa">
+              </div>
+              <div class="timeline-panel about-1">
+                <div class="timeline-heading">
+                  <!-- <h4>July 2014</h4> -->
+                  <h4 class="subheading text-uppercase">The Platforms and Reach-out Modalities</h4>
+                </div>
+                <div class="timeline-body">
+                    <ul>
+                      <li class="disc"><b>Annual BQa boot camp</b> - A 3 to 5 day mental emersion and psychological reboot. A camp that will have young boys learn people skills, group-study the scripture, explore purpose discovery, engage in skill development, learn the art of prayer, awareness on nation-building and many more</li>
+
+                      <li class="disc" id="theBoysCreed"><b>Secondary School Outreach and Mentorship Session</b> - A timely reach out to secondary school students, focusing on sexuality and academics</li>
+                      <span id="dots4"></span><span id="more4">
+                      <li class="disc"><b>BQa Campus Idea den</b> - The idea of a young African boy is supported and funded. This decision will be made in an Idea Den, where several ideas will be presented to a Murder board of other boys.</li>
+
+                      <li class="disc"><b>The BQ Tabloid </b>- Newsletters for young African boys.</li>
+
+                      <li class="disc"><b>The BQer Daily Confession hand-book</b> – A quarterly publication that embodies daily confession that reflects Leadership, sound mind and godliness</li>
+
+                      <li class="disc"><b>BQ Street Carnivals</b> - Open Games, Raffle Draws and Music on the street. Attracting the participation of young boys for engagements.</li>
+
+                      <li class="disc"><b>Boys, Bags and Tags (Back to School Outreach)</b> - Distribution of books and academics materials at the beginning of each session. This will be done at selected suburbs and local government across Africa</li><br></span>
+
+                      <button class="btn btn-primary" onclick="myFunction4()" id="myBtn4">Read More</button>
+                    </ul>     
+                </div>
+              </div>
+            </li>
+            <!-- Hidden only on xs, Hidden only on sm, Hidden only on md  (would show on large screens)-->
+            <li class="timeline-inverted d-none d-sm-block d-sm-none d-md-block d-md-none d-lg-block">
+              <div class="timeline-image creed-lg">
+                <a class="no-line" data-toggle="modal" href="#boysCreed">
+                  <div class="">
+                    <h3 class="text-white" style="padding-top:  35px">The<br>Boys<br>Creed</h3> 
+                </div>
+              </a>
+              </div>
+            </li>
+            <!-- Visible only on xs, Visible only on sm, Visible only on md (would show on small and medium screens)-->
+            <li class="timeline-inverted d-block d-sm-none d-none d-sm-block d-md-none">
+              <div class="timeline-image creed-sm">
+                <div class="col-md-4 col-sm-6">
+                  <a class="no-line" data-toggle="modal" href="#boysCreed">
+                    <h4 class="text-white" style="padding-top: 5px">The<br>Boys<br>Creed</h4>
+                  </a>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>`
+});
