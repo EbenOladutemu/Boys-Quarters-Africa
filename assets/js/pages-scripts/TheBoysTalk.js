a9c8c3f0c433d8e5edc4e97cb3dc19b6e2e7d20b
EbenDipo <e.oladutemu@taxtech.com.ng>
2019-08-23 13:27:38 +0100

    Updated Yahoo Project

---
+++ assets/js/pages-scripts/TheBoysTalk.js
@@ -0,0 +1,33 @@
+Vue.component('header-boys-talk',{
+	template: `<header class="masthead masthead-boys-talk article overlay-header">
+      <div class="container">
+        <div class="intro-text">
+          <!-- Hidden only on xs, Hidden only on sm, Hidden only on md -->
+          <div class="intro-lead-in d-none d-sm-block d-sm-none d-md-block d-md-none d-lg-block pad-lg-font-lg text-uppercase text-bold">We’ve been commissioned to educate, empower & advocate for a new tribe of Boys
+            who will deliberately & purposefully transition from Boyhood to Manhood.</div>
+          <!-- Visible only on xs, Visible only on sm -->
+          <div class="intro-lead-in d-block d-sm-none d-none d-sm-block d-md-none pad-sm-font-sm text-uppercase text-bold">We’ve been commissioned to educate, empower & advocate for a new tribe of Boys who will deliberately & purposefully transition from Boyhood to Manhood.</div>
+          <!-- <div class="intro-heading text-uppercase"></div> -->
+          <!-- <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger learn-mg" href="about-us">Learn More</a> -->
+        </div>
+      </div>
+    </header>`
+});
+
+Vue.component('the-boys-talk',{
+  template: `<section>
+    <div class="container">
+      <div class="col-lg-12 text-center">
+        <h2 class="section-heading text-uppercase">The Boys Talk</h2><br>
+      </div>
+      <div class="about-style">
+        <p> 
+          This is an annual Boy Only Conference and billed to be the largest across Africa and
+          billed to be the largest Boys-Only conference in world. The goal is to have Men
+          share their Success stories, struggles stories and experiences with the Boys. This
+          TED-like session exposes the mind of the Boys what leading oneself can do.
+        </p>
+      </div>
+    </div>
+  </section>`
+})
