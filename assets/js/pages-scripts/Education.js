edff3d2617ed1cbd155223a619368bce3e53c54d
EbenDipo <e.oladutemu@taxtech.com.ng>
2019-08-24 04:55:29 +0100

    Updated Education and Empowerment

---
+++ assets/js/pages-scripts/Education.js
@@ -0,0 +1,58 @@
+Vue.component('header-education',{
+	template: `<header class="masthead masthead-education article overlay-header">
+      <div class="container">
+        <div class="intro-text">
+          <!-- Hidden only on xs, Hidden only on sm, Hidden only on md -->
+          <div class="intro-lead-in d-none d-sm-block d-sm-none d-md-block d-md-none d-lg-block pad-lg-font-lg text-uppercase text-bold">We’ve been commissioned to educate, empower & advocate for a new tribe of Boys
+            who will deliberately & purposefully transition from Boyhood to Manhood.</div>
+          <!-- Visible only on xs, Visible only on sm -->
+          <div class="intro-lead-in d-block d-sm-none d-none d-sm-block d-md-none pad-sm-font-sm text-uppercase text-bold">We’ve been commissioned to educate, empower & advocate for a new tribe of Boys who will deliberately & purposefully transition from Boyhood to Manhood.</div>
+          <!-- <div class="intro-heading text-uppercase"></div> -->
+          <!-- <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger learn-mg" href="about-us">Learn More</a> -->
+        </div>
+      </div>
+    </header>`
+});
+
+Vue.component('education',{
+  template: `<section>
+    <div class="container">
+      <div class="col-lg-12 text-center">
+        <h2 class="section-heading text-uppercase">EDUCATION</h2><br>
+      </div>
+      <div class="about-style">
+        <p> 
+          Under the Pillar, Education, our goal is to empty the street of Africa of Young Boys who are subjected to
+          several form of violence. (Domestic, Emotional and Sexual).
+        </p>
+        <p>We are of the resolve that Every Child must Go to school.</p>
+        <p>
+          Boys aren’t Men yet. Hence, they shouldn’t be subjected to debasing realities that rid them off their Self
+          Esteem and desired future. However, our definition of Education here isn’t ABC or 123 only. It’s Music,
+          Drawing, Fashion, Dancing, etc. This is basically about transforming their Gifts to Skills and garnish it up
+          with Values.
+        </p>
+        <p>Through this pillar, we intend to reach out to disabled Boys- especially in the North.</p>
+        <h3>Flagship Projects</h3>
+        <p>
+          <b>Mobile Yahoo Hub</b>: This core objective of this project is combat the spike in internet fraud activities
+          amongst Boys and skill them up with the Skill of the Future.
+        </p>
+        <p>
+          This STEM project borders on our objective to skill up 2 Million under-privilege African Boys with skills
+          that are in tandem with their Counterparts across the world.
+        </p>
+        <p>
+          We’d be running Mobile Digital Hubs, where we’d take trainings on Artificial Intelligence, Data Science,
+          BlockChain Technology, Solutions Development, etc to their areas.
+        </p>
+        <p>
+          On this <b>Project ’YAHOO’</b>, we’d train Boys who are from below-humble backgrounds and skill them up to
+          put our names on Global Lips. This Project would see us take Mobile Yahoo Hub to different suburb
+          areas across Africa and they are also taught Critical Thinking & Problem Solving. Creativity &
+          Innovation.
+        </p>
+      </div>
+    </div>
+  </section>`
+})
