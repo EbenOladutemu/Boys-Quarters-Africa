8607db82bb271aa2239b6f678e2da0373e4e2c0e
EbenDipo <e.oladutemu@taxtech.com.ng>
2019-09-18 13:33:01 +0100

    Forms now using FormSubmit

--- assets/js/pages-scripts/contact/ContactForm.js
+++ assets/js/pages-scripts/contact/ContactForm.js
@@ -25,7 +25,7 @@ Vue.component('message',{
         </div>
         <div class="row">
           <div class="col-lg-12">
-            <form id="contactForm" name="sentMessage" novalidate="novalidate">
+            <form id="contactForm" action="https://formsubmit.co/bc945937359ec750bd113ec6f350cff5" name="sentMessage" novalidate="novalidate" method="POST">
               <div class="row">
                 <div class="col-md-6">
                   <div class="form-group">
