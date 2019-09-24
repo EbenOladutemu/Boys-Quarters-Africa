9c3b5dc9555c511746d8aaa56f18dea331347d5a
EbenDipo <e.oladutemu@taxtech.com.ng>
2019-09-24 10:00:30 +0100

    Added Subject input to contact form

--- assets/js/pages-scripts/contact/ContactForm.js
+++ assets/js/pages-scripts/contact/ContactForm.js
@@ -20,7 +20,7 @@ Vue.component('message',{
         <div class="row">
           <div class="col-lg-12 text-center">
             <h2 class="section-heading text-uppercase" style="margin-bottom: 50px">Contact Us</h2>
-            <!-- <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3> -->
+            <h3 class="section-subheading text-muted text-white" style="margin-bottom: 20px">Please kindly fill the form below for your enquiries, We love to hear from you.</h3>
           </div>
         </div>
         <div class="row">
@@ -28,8 +28,7 @@ Vue.component('message',{
             <form id="contactForm" name="sentMessage" novalidate="novalidate" method="POST">
               <div class="row">
                 <div class="col-md-6">
-                  <div class="form-group">
-                    <input type="hidden" id="_subject" name="_subject" value="New submission from BQA Contact Form">
+                  <div class="form-group">  
                     <input class="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name.">
                     <p class="help-block text-danger"></p>
                   </div>
@@ -38,13 +37,17 @@ Vue.component('message',{
                     <p class="help-block text-danger"></p>
                   </div>
                   <div class="form-group">
-                    <input class="form-control" id="phone" type="tel" placeholder="Your Phone *" required="required" data-validation-required-message="Please enter your phone number.">
+                    <input class="form-control" id="phone" type="tel" placeholder="Your Phone (optional)">
                     <p class="help-block text-danger"></p>
                   </div>
                 </div>
                 <div class="col-md-6">
+                  <div class="form-group">  
+                    <input class="form-control" id="_subject" name="_subject" type="text" placeholder="Subject *" required="required" data-validation-required-message="Please enter a subject.">
+                    <p class="help-block text-danger"></p>
+                  </div>
                   <div class="form-group">
-                    <textarea class="form-control no-resize" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
+                    <textarea class="form-control" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
                     <p class="help-block text-danger"></p>
                   </div>
                 </div>
