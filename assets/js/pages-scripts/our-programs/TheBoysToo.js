bb31ce87160ff749d3793956ab132488324518f1
EbenDipo <olakennyfemi@gmail.com>
2019-11-25 23:11:24 +0100

    Remove #BoysToo preloader

--- assets/js/pages-scripts/our-programs/TheBoysToo.js
+++ assets/js/pages-scripts/our-programs/TheBoysToo.js
@@ -109,10 +109,10 @@ Vue.component('boys-too',{
           </div>
         </div>
       </div>
-      <div id="loader-wrapper"> 
+      <!-- <div id="loader-wrapper"> 
         <div id="loader"></div>
         <div class="loader-section section-left"></div>
         <div class="loader-section section-right"></div>
-      </div>
+      </div> -->
     </section>`
 })
