04e7555131ac2040892891dae9e321d7ee97aa72
Ebenezer Oladutemu <eoladutemu@parkwayprojects.com>
2020-10-19 18:01:10 +0100

    Commit whatever it is can be committed

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,44 @@
 module.exports = {
+  css: {
+    sourceMap: true
+  },
   configureWebpack: {
-    devtool: 'source-map'
-  }
+    devtool: 'source-map',
+  },
+  pwa: {
+    name:"Boys Quarters Africa",
+    shortName:"BQA",
+    themeColor:"#212856",
+    description: "We are a Movement resolved to educate, empower & advocate for a new tribe of boys who will deliberately & purposefully transition from Boyhood to Manhood.",
+    icons:[
+       {
+          src:"./img/icons/android-chrome-192x192.png",
+          sizes:"192x192",
+          type:"image/png"
+       },
+       {
+          src:"./img/icons/android-chrome-512x512.png",
+          sizes:"512x512",
+          type:"image/png"
+       },
+       {
+          src:"./img/icons/android-chrome-maskable-192x192.png",
+          sizes:"192x192",
+          type:"image/png",
+          purpose:"maskable"
+       },
+       {
+          src:"./img/icons/android-chrome-maskable-512x512.png",
+          sizes:"512x512",
+          type:"image/png",
+          purpose:"maskable"
+       }
+    ],
+    startUrl:"./index.html",
+    display:"standalone",
+    backgroundColor:"#000000",
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+ }
 }
